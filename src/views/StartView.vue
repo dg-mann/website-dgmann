2b07599303b4d60c16658ace46cbea7d01af8ab4
DGMANN <paul.deg37@gmail.com>
2024-05-13 18:35:52 +0000

    added vue project

---
+++ src/views/StartView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>DGMANN.</h1>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/font-faces.scss';
+
+h1 {
+  font-family: 'Shrikhand Regular';
+}
+
+</style>
