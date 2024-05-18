e84e7b88b08e27afe40596b275f35d73bd8eb2f1
DGMANN <paul.deg37@gmail.com>
2024-05-18 09:34:35 +0000

    added grid for image gallery

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import Footer from '@/components/Footer.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="50" />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
 
   <RouterView />
+  <Footer />
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- src/components/ImageBoard.vue
+++ src/components/ImageBoard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="grid-container">
-    <div class="image-item" v-for="image in imageList" :key="image">
-      <img :src="image" />
+  <div class="image-gallery">
+    <div class="image-gallery__image-item" v-for="image in imageList" :key="image">
+      <img :src="image" class="image-gallery__image" />
     </div>
   </div>
 </template>
@@ -9,23 +9,42 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 const imageList = ref([
-  'https://picsum.photos/id/65/200/300.jpg',
-  'https://picsum.photos/id/65/200/400.jpg',
-  'https://picsum.photos/id/65/200/500.jpg'
+  'https://picsum.photos/id/65/100/50.jpg',
+  'https://picsum.photos/id/65/100/50.jpg',
+  'https://picsum.photos/id/65/100/500.jpg',
+  'https://picsum.photos/id/65/100/50.jpg',
+  'https://picsum.photos/id/65/100/50.jpg',
+  'https://picsum.photos/id/65/100/50.jpg',
+  'https://picsum.photos/id/65/100/50.jpg',
+  'https://picsum.photos/id/65/100/50.jpg'
 ])
 </script>
 
 <style lang="scss" scoped>
-.grid-container {
+.image-gallery {
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-}
+  grid-template-columns: repeat(1, 1fr);
+  grid-template-rows: repeat(8, 80vw);
 
-.image-item {
-  display: flex;
-  justify-content: center;
-  grid-column: span 1;
-  grid-row: span 1;
+  @media screen and (min-width: 500px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(8, 40vw);
+  }
+
+  @media screen and (min-width: 800px) {
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(8, 20vw);
+  }
+
+  @media screen and (min-width: 1200px) {
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(8, 10vw);
+  }
+
+  &__image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 }
 </style>
